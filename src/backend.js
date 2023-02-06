4be70fd1cd6217a533d733d477b2a2af80ccf762
hugo <75113230+hug-efrei@users.noreply.github.com>
2023-02-06 11:40:57 +0100

    Working backend API

--- src/backend.js
+++ src/backend.js
@@ -4,13 +4,6 @@ const cors = require("cors");
 
 const app = express();
 
-// db connection
-const db = require("./models");
-
-db.sequelize.sync({ force: true }).then(() => {
-    console.log("Drop and re-sync db.");
-  });
-
 var corsOptions = {
   origin: "http://localhost:8081"
 };
@@ -23,6 +16,14 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = require("./models");
+const Role = db.role;
+
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to agorachain application." });
@@ -33,4 +34,26 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+// routes
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
+
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+   
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
 
