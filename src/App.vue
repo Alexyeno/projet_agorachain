ced8696475159cec1ed25a68e74e740a0c213672
EvanMounaud <evan.mounaud@efrei.net>
2023-02-25 12:55:08 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@ import Sidebar from './components/Sidebar.vue';
 :root {
 	--primary: #8347E5;
 	--primary-alt: #E0DBFB;
+  --primary-background: #E0DBFB;
 	--grey: #64748b;
 	--dark: #1e293b;
 	--dark-alt: #334155;
